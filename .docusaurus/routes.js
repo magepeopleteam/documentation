556bca458145b6b52eb7026d00e4e11f031968d4
Shahadat <raselsha@gmail.com>
2026-07-08 13:23:33 +0600

    update plugin order

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -39,15 +39,15 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs', 'ff2'),
+    component: ComponentCreator('/docs', '044'),
     routes: [
       {
         path: '/docs',
-        component: ComponentCreator('/docs', '5d6'),
+        component: ComponentCreator('/docs', 'a12'),
         routes: [
           {
             path: '/docs',
-            component: ComponentCreator('/docs', 'd7c'),
+            component: ComponentCreator('/docs', '22b'),
             routes: [
               {
                 path: '/docs/addons/bank-payment-gateway/overview',
@@ -224,32 +224,8 @@ export default [
                 sidebar: "docsSidebar"
               },
               {
-                path: '/docs/category/wpequipments',
-                component: ComponentCreator('/docs/category/wpequipments', '657'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
                 path: '/docs/category/wpevently',
                 component: ComponentCreator('/docs/category/wpevently', 'f12'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/category/wpfoodly',
-                component: ComponentCreator('/docs/category/wpfoodly', '4c3'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/category/wpgrouply',
-                component: ComponentCreator('/docs/category/wpgrouply', '2fc'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/category/wpgymly',
-                component: ComponentCreator('/docs/category/wpgymly', '5dd'),
                 exact: true,
                 sidebar: "docsSidebar"
               },
@@ -626,42 +602,6 @@ export default [
                 sidebar: "docsSidebar"
               },
               {
-                path: '/docs/plugins/wpequipments/changelog',
-                component: ComponentCreator('/docs/plugins/wpequipments/changelog', 'c00'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpequipments/faq',
-                component: ComponentCreator('/docs/plugins/wpequipments/faq', '855'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpequipments/features',
-                component: ComponentCreator('/docs/plugins/wpequipments/features', 'add'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpequipments/overview',
-                component: ComponentCreator('/docs/plugins/wpequipments/overview', 'b7a'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpequipments/settings',
-                component: ComponentCreator('/docs/plugins/wpequipments/settings', 'b41'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpequipments/shortcodes',
-                component: ComponentCreator('/docs/plugins/wpequipments/shortcodes', '679'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
                 path: '/docs/plugins/wpevently/changelog',
                 component: ComponentCreator('/docs/plugins/wpevently/changelog', '33f'),
                 exact: true,
@@ -730,114 +670,6 @@ export default [
               {
                 path: '/docs/plugins/wpevently/video-tutorials',
                 component: ComponentCreator('/docs/plugins/wpevently/video-tutorials', 'd3a'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpfoodly/changelog',
-                component: ComponentCreator('/docs/plugins/wpfoodly/changelog', '1e5'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpfoodly/faq',
-                component: ComponentCreator('/docs/plugins/wpfoodly/faq', '581'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpfoodly/features',
-                component: ComponentCreator('/docs/plugins/wpfoodly/features', '0dd'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpfoodly/overview',
-                component: ComponentCreator('/docs/plugins/wpfoodly/overview', '972'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpfoodly/settings',
-                component: ComponentCreator('/docs/plugins/wpfoodly/settings', '2a1'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpfoodly/shortcodes',
-                component: ComponentCreator('/docs/plugins/wpfoodly/shortcodes', 'fda'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpgrouply/changelog',
-                component: ComponentCreator('/docs/plugins/wpgrouply/changelog', '8b8'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpgrouply/faq',
-                component: ComponentCreator('/docs/plugins/wpgrouply/faq', '860'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpgrouply/features',
-                component: ComponentCreator('/docs/plugins/wpgrouply/features', 'f21'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpgrouply/overview',
-                component: ComponentCreator('/docs/plugins/wpgrouply/overview', '620'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpgrouply/settings',
-                component: ComponentCreator('/docs/plugins/wpgrouply/settings', '759'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpgrouply/shortcodes',
-                component: ComponentCreator('/docs/plugins/wpgrouply/shortcodes', '4e9'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpgymly/changelog',
-                component: ComponentCreator('/docs/plugins/wpgymly/changelog', '2e0'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpgymly/faq',
-                component: ComponentCreator('/docs/plugins/wpgymly/faq', 'b0d'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpgymly/features',
-                component: ComponentCreator('/docs/plugins/wpgymly/features', 'af2'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpgymly/overview',
-                component: ComponentCreator('/docs/plugins/wpgymly/overview', 'bd4'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpgymly/settings',
-                component: ComponentCreator('/docs/plugins/wpgymly/settings', 'ade'),
-                exact: true,
-                sidebar: "docsSidebar"
-              },
-              {
-                path: '/docs/plugins/wpgymly/shortcodes',
-                component: ComponentCreator('/docs/plugins/wpgymly/shortcodes', 'e3d'),
                 exact: true,
                 sidebar: "docsSidebar"
               },
