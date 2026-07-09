642af0867a99fb404c2acdbda99be760386207bb
Shahadat <raselsha@gmail.com>
2026-07-09 11:52:06 +0600

    Refactor code structure for improved readability and maintainability

--- .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
+++ .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
@@ -1,6 +1,6 @@
 import lunr from "/var/www/html/documentation/node_modules/lunr/lunr.js";
 export const removeDefaultStopWordFilter = [];
 export const language = ["en"];
-export const searchIndexUrl = "search-index{dir}.json?_=ec27c7e5";
+export const searchIndexUrl = "search-index{dir}.json?_=b694bb4b";
 export const searchResultLimits = 8;
 export const fuzzyMatchingDistance = 1;
